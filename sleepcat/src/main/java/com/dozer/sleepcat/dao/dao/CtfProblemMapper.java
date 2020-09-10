8373749cc7b079dd8b051292d6ea5a5dab1ae321
Edgaraaa <17384592226@163.com>
2020-09-10 17:32:38 +0800

    helloworld

--- sleepcat/src/main/java/com/dozer/sleepcat/dao/dao/CtfProblemMapper.java
+++ sleepcat/src/main/java/com/dozer/sleepcat/dao/dao/CtfProblemMapper.java
@@ -1,4 +1,10 @@
 package com.dozer.sleepcat.dao.dao;
 
+
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface CtfProblemMapper {
+   int selectByPID(Integer pid);
+
 }
