0a912a0a34f39ccec0176564e5cf66da87792c9f
Edgaraaa <17384592226@163.com>
2020-09-29 17:15:25 +0800

    123

--- sleepcat/src/main/java/com/dozer/sleepcat/SleepcatApplication.java
+++ sleepcat/src/main/java/com/dozer/sleepcat/SleepcatApplication.java
@@ -1,11 +1,13 @@
 package com.dozer.sleepcat;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@MapperScan("com.dozer.sleepcat.dao.dao")
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class SleepcatApplication {
 
